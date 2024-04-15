a8a0de2a6302fb9f4dee6476191c2740410efc39
Daniel <dani.07rojas94@gmail.com>
2024-04-15 17:48:46 -0500

    adicionando

---
+++ src/functions.js
@@ -0,0 +1,7 @@
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+
+export function show_alert(title, message){
+    const MySwal = withReactContent(Swal);
+    
+}
