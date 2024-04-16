d762f3661f0ccfebd701ecf592d82cf8987f5ba4
Daniel <dani.07rojas94@gmail.com>
2024-04-15 22:16:47 -0500

    nuevos archivos

---
+++ src/components/ShowProducts.js
@@ -0,0 +1,14 @@
+import React,{useEffect, useState} from "react";
+import axios from "axios";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+import { show_alert } from "../functions";
+
+const ShowProducts = () => {
+    const url = "https://localhost:8080";
+    return (
+        <div>
+
+        </div>
+    )
+}
