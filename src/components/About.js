c6b3eee1a0ffc9b71c3c976a02a79338000da763
MiltonGutierrez <milton.gutierrez-l@mail.escuelaing.edu.co>
2024-04-19 18:33:19 -0500

    Componente ABOUT

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div className='about-container'>
+      <h1>SOBRE NOSOTROS</h1>
+      <h3>Celebramos el buen estilo</h3>
+      <p>Tu armario virtual está aquí para ayudar a compradores como tú a encontrar la ropa que les gusta sin comprometer la calidad. Seleccionamos cuidadosamente cada pieza de nuestras colecciones a fin de que cumpla con nuestros altos estándares. Nuestra ropa se siente excepcional porque está diseñada por expertos para ajustarse bien, durar mucho y lucir genial. Compra hoy y compruébalo tú mismo.</p>
+    </div>
+  );
+};  
+export default About;
