29b77bb1814cabc460ef9e670d9b14106d83aee3
Sebastian Sosa <jhon.sosa-m@mail.escuelaing.edu.co>
2024-04-26 20:57:31 -0500

    Cambios en el testimonio #1

--- src/components/paginaPrincipal/Testimonios.js
+++ src/components/paginaPrincipal/Testimonios.js
@@ -1,23 +1,21 @@
-import  '../../styleSheets/Testimonio.css';
 import React from 'react';
-import imagenM from '../../images/imagenM.avif'
+import imagenM from '../../images/imagenM.avif';
+import '../../styleSheets/Testimonio.css';
 
-export function Testimonios(){
-    return (
-        <div className='Testimonio' data-testid = "testimonios-1">
-                <div className='Imagen'>
-                    <img src={imagenM} height='40%' width='55%'alt = "Imagen de moda"/>
-                </div>
-            <div className='Contenedor-texto'>
-                <p className='NombrePersona'>Nombre Persona</p>
-                <p className='TextoTestimonio'>
-¡Claro!
-
-"¡Finalmente encontré la solución perfecta para mis dilemas diarios de qué ponerme! Con esta aplicación de armario virtual, puedo organizar toda mi ropa de manera fácil y rápida. Ya no paso horas frente al armario tratando de decidir qué usar. Ahora, con solo unos pocos toques en mi teléfono, puedo ver todas mis opciones de ropa y armar el look perfecto en minutos. ¡Es como tener un estilista personal en mi bolsillo! ¡Gracias por simplificar mi vida diaria y hacer que vestirme sea divertido de nuevo!"</p>
-            </div>
-        </div>
-        
-    );
-};
+function Testimonios() {
+  return (
+    <div className='Testimonio' data-testid="testimonios-1">
+      <div className='Imagen'>
+        <img src={imagenM} height='40%' width='55%' alt="Imagen de moda"/>
+      </div>
+      <div className='Contenedor-texto'>
+        <p className='TextoTestimonio'>
+          ¡Finalmente encontré la solución perfecta para mis dilemas diarios de qué ponerme! Con esta aplicación de armario virtual, puedo organizar toda mi ropa de manera fácil y rápida. Ya no paso horas frente al armario tratando de decidir qué usar. Ahora, con solo unos pocos toques en mi teléfono, puedo ver todas mis opciones de ropa y armar el look perfecto en minutos. ¡Es como tener un estilista personal en mi bolsillo! ¡Gracias por simplificar mi vida diaria y hacer que vestirme sea divertido de nuevo!
+        </p>
+        <p className='NombrePersona'>Juan García</p>
+      </div>
+    </div>
+  );
+}
 
 export default Testimonios;
