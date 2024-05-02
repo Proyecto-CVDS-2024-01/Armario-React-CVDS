705f94fee387e865d35bd111bbf44106586721b8
Sebastian Sosa <jhon.sosa-m@mail.escuelaing.edu.co>
2024-05-02 15:53:35 -0500

    Cambios para subir #2

--- src/components/paginaProducto/imagenesCuerpo/Clean.js
+++ src/components/paginaProducto/imagenesCuerpo/Clean.js
@@ -3,7 +3,7 @@ import '../../../styleSheets/ProductPage.css'
 const Clean = () => {
   return (
     <div className='clean-container' data-testid="tshirt-1">
-      <h1></h1>
+      <h1>&nbsp;</h1>
     </div>
   )
 }
