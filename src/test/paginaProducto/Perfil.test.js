fad1ea8cb82b23185900011dba99abc25e6bcadc
MiltonGutierrez <milton.gutierrez-l@mail.escuelaing.edu.co>
2024-05-21 15:35:29 -0500

    Creacion de archivos .test.js para cada uno de los componentes existentes, (FALTA LÓGICA) #8

---
+++ src/test/paginaProducto/Perfil.test.js
@@ -0,0 +1,12 @@
+/*
+import {render, screen} from '@testing-library/react'
+import About from '../../components/paginaPrincipal/About'
+import '@testing-library/jest-dom'
+test('should render Aboutoo component', () => { 
+    render(<About/>);
+    const aboutElement = screen.getByTestId('about-1');
+    expect(aboutElement).toBeInTheDocument();
+    expect(aboutElement).toHaveTextContent('SOBRE NOSOTROS');
+})
+FALTA
+*/ 
